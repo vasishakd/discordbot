56f14c8679e0775b0fc284c8590988eee3432390
Vasishak Denis <vasishak.d@gmail.com>
2018-09-09 18:50:21 +0500

    role check

--- bot.js
+++ bot.js
@@ -24,53 +24,54 @@ let timer;
 // }
 
 function startTimerGG() {
-	let xhr = new XMLHttpRequest();
-	xhr.open("GET", 'http://api2.goodgame.ru/streams/' + botconfig.channel_gg, true);
-	xhr.setRequestHeader('Accept', 'application/json');
-	xhr.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			data = JSON.parse(xhr.responseText);
-			if (data.status == 'Live' && isLive == false) {
-				client.channels.get(botconfig.channel_discord).send('@everyone '+ data.url + ' go live');
-				isLive = true;
-			}
-			if (data.status == 'Dead' && isLive == true) {
-				isLive = false;
-			}
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", 'http://api2.goodgame.ru/streams/' + botconfig.channel_gg, true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            data = JSON.parse(xhr.responseText);
+            if (data.status == 'Live' && isLive == false) {
+                client.channels.get(botconfig.channel_discord).send('@everyone ' + data.url + ' go live');
+                isLive = true;
+            }
+            if (data.status == 'Dead' && isLive == true) {
+                isLive = false;
+            }
         }
-	};
-	xhr.send();
+    };
+    xhr.send();
 }
 
 function rewriteJson(discord, channel) {
-  let content;
-  let contents = fs.readFileSync('botconfig.json', 'utf8');
-  content = JSON.parse(contents);
-  content.channel_discord = discord;
-  content.channel_gg = channel;
-  content = JSON.stringify(content);
-  fs.writeFileSync("botconfig.json", content);
+    let content;
+    let contents = fs.readFileSync('botconfig.json', 'utf8');
+    content = JSON.parse(contents);
+    content.channel_discord = discord;
+    content.channel_gg = channel;
+    content = JSON.stringify(content);
+    fs.writeFileSync("botconfig.json", content);
 
 }
 
 client.on('ready', () => {
-  timer = setInterval(startTimerGG, 10000);
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("Alive");
+    timer = setInterval(startTimerGG, 10000);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("Alive");
 });
 
 client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-  let params = message.content.split(' ');
-  if (message.content === prefix + 'ping') {
-    message.channel.send(message.channel.id);
-  }
-  if (params.length == 2 && params[0] == prefix + 'notify') {
-    rewriteJson(message.channel.id, params[1]);
-    message.channel.send('Канал '+ params[1] + ' отслеживается');
-    botconfig = botconfig = JSON.parse(fs.readFileSync('botconfig.json', 'utf8'));
-  }
-  });
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    let params = message.content.split(' ');
+    if (message.content === prefix + 'ping') {
+        message.channel.send(message.channel.id);
+    }
+    if (params.length == 2 && params[0] == prefix + 'notify'
+        && message.member.roles.some(r => ["moderator"].includes(r.name))) {
+        rewriteJson(message.channel.id, params[1]);
+        message.channel.send('Канал ' + params[1] + ' отслеживается');
+        botconfig = JSON.parse(fs.readFileSync('botconfig.json', 'utf8'));
+    }
+});
 
 client.login(botconfig.token);
 
