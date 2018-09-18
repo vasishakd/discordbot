f95d9677a2b1620401a92f9485c1e53425e4644a
Vasishak Denis <vasishak.d@gmail.com>
2018-09-18 20:51:57 +0500

    zahar

--- bot.js
+++ bot.js
@@ -54,10 +54,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.member.user.tag == 'Захар#6967' 
-            || message.member.user.tag == '3axap#6967'
-            || message.member.user.tag == '3ахар#6967' 
-            || message.member.user.tag == 'derKotik#3467') {
+    if (message.member.user.tag == '3axap#6967') {
         const emoji = message.guild.emojis.find('name', 'hahaa');
         message.react(emoji);
     }
