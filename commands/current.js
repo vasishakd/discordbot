2631e6fb6e29f85e479c9b4c0ddf0ad83bd1859b
Vasishak Denis <vasishak.d@gmail.com>
2018-10-21 20:58:54 +0500

    current command add link to gg

--- commands/current.js
+++ commands/current.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'current',
     description: 'Current notify channel.',
     execute(client, message, args, botconfig) {
-        message.channel.send('Текущий отслеживаемый канал: ' + botconfig.channel_gg);
+        message.channel.send('Текущий отслеживаемый канал: https://goodgame.ru/channel/' + botconfig.channel_gg);
     },
 };
