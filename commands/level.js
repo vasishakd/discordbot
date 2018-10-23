0e32ce659e16c0763a45add5fe25cd2769b6a2a9
Vasishak Denis <vasishak.d@gmail.com>
2018-10-24 01:17:21 +0500

    fix xp required?

--- commands/level.js
+++ commands/level.js
@@ -1,9 +1,8 @@
-let xp = require('../xp.json');
-
 module.exports = {
     name: 'level',
     description: 'Your current level.',
     execute(client, message) {
+        let xp = require('../xp.json');
         let xpRemain = xp[message.author.id].level * 250 - xp[message.author.id].xp;
         message.channel.send('Your level: ' + xp[message.author.id].level + '. Exp for next level: ' + xpRemain);
     },
